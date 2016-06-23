1f8596007f194ebe296cdc6b86062201be250318
wzsxyz <boyxyz@126.com>
2016-06-23 10:14:56 +0800

    修复bug

--- index.js
+++ index.js
@@ -5,26 +5,30 @@ var loaderUtils = require("loader-utils");
 
 module.exports = function(fileContent) {
 	var query = loaderUtils.parseQuery(this.query);
-	if(query.deep !== false) fileContent = loadDeep(fileContent, this.context, this.query);
-	return "module.exports = '"+replaceSrc(fileContent, query.min)+"'";
+	fileContent = query.min === false?fileContent.replace(/\n/g, '\\n'):fileContent.replace(/\n/g, '');
+	fileContent = JSON.stringify(fileContent);
+	if(query.deep !== false) fileContent = loadDeep(fileContent, this.query);
+	return "module.exports = "+replaceSrc(fileContent);
 };
 
 
-function replaceSrc(fileContent, min) {
-	fileContent = min === false?fileContent.replace(/\n/g, '\\n'):fileContent.replace(/\n/g, '');
-	fileContent = fileContent.replace(/\<img[^\<\>]+?src=[\"\']?\.?\.?\/[^\'\"\<\>]+?[\'\"][^\<\>]*?\>/g, function(str){
-		var reg = /src=[\'\"][^\"\']+[\'\"]/i;
+function replaceSrc(fileContent) {
+	fileContent = fileContent.replace(/\<img[^\<\>]+?src=\\?[\"\']?(\.?\.?\/)?[^\'\"\<\>]+?\\?[\'\"][^\<\>]*?\>/g, function(str){
+		var reg = /src=\\?[\'\"][^\"\']+\\?[\'\"]/i;
 		var regResult = reg.exec(str);
-		var imgUrl = regResult[0].replace('src=', '').replace(/\\|\'|\"/g, '');
-		return str.replace(reg, "src=\"\'+require(\""+imgUrl+"\")+\'\"");
+		var imgUrl = regResult[0].replace('src=', '').replace(/[\\\'\"]/g, '');
+		if(!(/^[\.\/]/).test(imgUrl)) {
+			imgUrl = './' + imgUrl;
+		}
+		return str.replace(reg, "src=\"+require("+JSON.stringify(imgUrl)+")+\"");
 	});
 	return fileContent;
 }
 
-function loadDeep(fileContent, context, queryStr) {
-	return fileContent.replace(/#include\(['"][^'"]+['"]\);?/g, function(str){
-		var childFileSrc = str.replace(/[\'\"\>\(\);]/g, '').replace('#include', '');
-		return "\'+require(\"html-withimg-loader"+queryStr+"!"+childFileSrc+"\")+\'";
+function loadDeep(fileContent, queryStr) {
+	return fileContent.replace(/#include\(\\?[\'\"][^\'\"]+\\?[\'\"]\);?/g, function(str){
+		var childFileSrc = str.replace(/[\\\'\"\>\(\);]/g, '').replace('#include', '');
+		return "\"+require("+JSON.stringify("html-withimg-loader"+queryStr+"!"+childFileSrc)+")+\"";
 	});
 }
 
